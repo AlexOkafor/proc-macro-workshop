4eed8abf731b5a148b5ce976929aa39fe7f016ea
Alex Okafor <okafora@gmail.com>
2022-12-14 19:40:12 -0500

    stubbing out derive

--- builder/src/lib.rs
+++ builder/src/lib.rs
@@ -1,8 +1,16 @@
 use proc_macro::TokenStream;
+use quote::ToTokens;
+use syn::DeriveInput;
 
 #[proc_macro_derive(Builder)]
 pub fn derive(input: TokenStream) -> TokenStream {
     let _ = input;
+    let ast: DeriveInput = syn::parse(input).unwrap();
 
-    unimplemented!()
+    derive_impl(&ast);
+    TokenStream::new()
+}
+
+fn derive_impl(ast: &DeriveInput) -> TokenStream {
+    TokenStream::from(ast.to_token_stream())
 }
